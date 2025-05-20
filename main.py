fab6fd728257ec0b519db67b7359465bc63f7cea
abimanyuprimarendra <139678948+abimanyuprimarendra@users.noreply.github.com>
2025-05-20 21:24:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 import matplotlib.pyplot as plt
+import gdown
 
 @st.cache_data
 def load_data():
