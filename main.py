387a07880e6eeb3b5b7e7cdc101e19e6e72d6a31
abimanyuprimarendra <139678948+abimanyuprimarendra@users.noreply.github.com>
2025-07-15 00:44:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ if cari:
             border-radius: 15px;
             padding: 16px;
             box-shadow: 0 4px 8px rgba(0,0,0,0.1);
-            height: 300px;
+            height: 500px;
             display: flex;
             flex-direction: column;
             justify-content: flex-start;
